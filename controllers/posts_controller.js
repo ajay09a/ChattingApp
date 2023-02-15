674bdf032a26d96f7d03635dfe0e5711d81faac7
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-02-15 16:19:42 +0530

    second commit: add async and await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,29 +1,64 @@
 const Post = require('../models/post')
 const Comment = require('../models/comment')
 
-module.exports.create = function(req, res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){console.log('error in creating a post'); return;}
+// module.exports.create = function(req, res){
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     }, function(err, post){
+//         if(err){console.log('error in creating a post'); return;}
 
+//         return res.redirect('back');
+//     });
+// }
+
+
+// module.exports.destroy = function(req, res){
+//     Post.findById(req.params.id, function(err, post){
+//         // .id means converting the object id into string
+//         if(post.user == req.user.id){
+//             post.remove();
+
+//             Comment.deleteMany({post: req.params.id}, function(err){
+//                 return res.redirect('back');
+//             })
+//         }
+//         else{
+//             return res.redirect('back');
+//         }
+//     })
+// }
+
+module.exports.create = async function(req, res){
+    try {
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        })
+        req.flash('success', 'Content Posted');
         return res.redirect('back');
-    });
+    } catch (error) {
+        console.log("Error");
+        return;
+    }
 }
 
-module.exports.destroy = function(req, res){
-    Post.findById(req.params.id, function(err, post){
-        // .id means converting the object id into string
+module.exports.destroy = async function(req, res){
+    try {
+        let post = await Post.findById(req.params.id)
+
         if(post.user == req.user.id){
             post.remove();
 
-            Comment.deleteMany({post: req.params.id}, function(err){
-                return res.redirect('back');
-            })
+            let deleteComment = await Comment.deleteMany({post: req.params.id})
+            req.flash('success', 'Post Deleted Successfully');
+            return res.redirect('back');
         }
         else{
             return res.redirect('back');
         }
-    })
+    } catch (error) {
+        console.log("Error");
+        return;
+    }
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,22 +1,31 @@
 const User = require('../models/user');
 
 
-module.exports.profile = function(req, res){
-    User.findById(req.params.id, function(err, user){
+module.exports.profile = async function(req, res){
+    try {
+        let user = await User.findById(req.params.id)
         return res.render('user_profile', {
             title: 'User Profile',
             profile_user: user
         })
-    })
+    } catch (error) {
+        console.log("Error");
+        return;
+    }
 }
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+module.exports.update = async function(req, res){
+    try {
+        if(req.user.id == req.params.id){
+            let user = await User.findByIdAndUpdate(req.params.id, req.body)
+            req.flash('success', 'Profile updated successfully');
             return res.redirect('back');
-        });
-    }else{
-        return res.status(401).send('Unauthorized');
+        }else{
+            return res.status(401).send('Unauthorized');
+        }
+    } catch (error) {
+        console.log("Error");
+        return;
     }
 }
 
@@ -46,32 +55,39 @@ module.exports.signIn = function(req, res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
-        return res.redirect('back');
-    }
-
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
-
+module.exports.create = async function(req, res){
+    try {
+        if (req.body.password != req.body.confirm_password){
+            req.flash('success', 'You have written different password');
+            return res.redirect('back');
+        }
+    
+        let user = await User.findOne({email: req.body.email})
+    
         if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
+            let user = await User.create(req.body)
+            req.flash('success', 'Account Created');
+            return res.redirect('/users/sign-in');
         }else{
             return res.redirect('back');
         }
-
-    });
+    
+    } catch (error) {
+        console.log("Error");
+        return;
+    }
 }
 
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
-    req.flash('success', 'Logged in Successfully');
-    return res.redirect('/');
+module.exports.createSession = async function(req, res){
+    try {
+        req.flash('success', 'Logged in Successfully');
+        return res.redirect('/');
+    } catch (error) {
+        console.log("Error");
+        return;
+    }
 }
 
 module.exports.destroySession = function(req, res){
