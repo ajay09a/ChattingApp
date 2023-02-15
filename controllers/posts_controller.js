b777c9bb25cc69aba198576a2923724f70643ea8
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-02-15 21:42:44 +0530

    third commit: sending data ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -35,6 +35,16 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         })
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "post created"
+            })
+        }
+
         req.flash('success', 'Content Posted');
         return res.redirect('back');
     } catch (error) {
